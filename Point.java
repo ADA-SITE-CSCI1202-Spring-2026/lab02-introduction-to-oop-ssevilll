dc55251e0d47e299257d4c89f577742ba0e59710
Sevil Babayeva <sevilsb@code.edu.az>
2026-02-12 10:36:54 +0400

    initial

---
+++ Point.java
@@ -0,0 +1,31 @@
+public class Point {
+    private  float x;
+    private  float y;
+    public Point(){
+        x = 0f;
+        y = 0f;
+    }
+    public Point(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p){
+        this.x = p.x;
+        this.y = p.y;
+    }
+
+    public float getX(){
+        return x;
+    }
+    public float getY(){
+        return y;
+    }
+    public void setX(float x){
+        this.x = x;
+    }
+    public void setY(float y){
+        this.y = y;
+    }
+
+}
